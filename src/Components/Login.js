51ab4e978f370cbb4b857036497bbf33f2849767
Anthony <aseungkilee@gmail.com>
2019-05-10 14:17:39 -0400

    added conditional forms

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,65 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Reg_Form from "./Reg_Form.js";
 
-class Login extends Component {
-  state = {
-    username: null,
-    email: null,
-    phone: null,
-    password: null
-  };
-
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-    console.log(this.state);
-  };
-
-  loginHandler = event => {
-    event.preventDefault();
-    this.props.register(
-      this.state.username,
-      this.state.email,
-      this.state.phone,
-      this.state.password
-    );
-  };
-
-  render() {
-    return (
-      <div>
-        <form autoComplete="off" onSubmit={this.loginHandler}>
-          <ul>
-            <li>
-              <input
-                onChange={this.handleChange}
-                name="username"
-                placeholder="Username"
-              />
-            </li>
-            <li>
-              <input
-                onChange={this.handleChange}
-                name="password"
-                placeholder="Password"
-                type="password"
-              />
-            </li>
-            <input type="submit" />
-          </ul>
-        </form>
-      </div>
-    );
-  }
-}
-
-// const mapStateToProps = state => {
-//   console.log("store state", state);
-//   return {
-//     currentUser: state.currentUser,
-//     result: state.result
-//   };
-// };
+const Login = props => {
+  return (
+    <div>
+      <Reg_Form />
+    </div>
+  );
+};
 
 export default Login;
--- src/Components/Reg_Form.js
+++ src/Components/Reg_Form.js
@@ -21,7 +21,7 @@ class Registration extends Component {
     console.log(this.state);
   };
 
-  submitHandler = event => {
+  registrationHandler = event => {
     event.preventDefault();
     this.props.register(
       this.state.username,
@@ -32,45 +32,71 @@ class Registration extends Component {
   };
 
   render() {
-    return (
-      <div>
-        {console.log("after submit", this.props.result)}
-        <form autoComplete="off" onSubmit={this.submitHandler}>
-          <ul>
-            <li>
-              <input
-                onChange={this.handleChange}
-                name="username"
-                placeholder="Username"
-              />
-            </li>
-            <li>
-              <input
-                onChange={this.handleChange}
-                name="email"
-                placeholder="Email"
-              />
-            </li>
-            <li>
-              <input
-                onChange={this.handleChange}
-                name="phone"
-                placeholder="Phone"
-              />
-            </li>
-            <li>
-              <input
-                onChange={this.handleChange}
-                name="password"
-                placeholder="Password"
-                type="password"
-              />
-            </li>
-            <input type="submit" />
-          </ul>
-        </form>
-      </div>
-    );
+    if (this.props.registerComponent === true) {
+      return (
+        <div>
+          <form autoComplete="off" onSubmit={this.registrationHandler}>
+            <ul>
+              <li>
+                <input
+                  onChange={this.handleChange}
+                  name="username"
+                  placeholder="Username"
+                />
+              </li>
+              <li>
+                <input
+                  onChange={this.handleChange}
+                  name="email"
+                  placeholder="Email"
+                />
+              </li>
+              <li>
+                <input
+                  onChange={this.handleChange}
+                  name="phone"
+                  placeholder="Phone"
+                />
+              </li>
+              <li>
+                <input
+                  onChange={this.handleChange}
+                  name="password"
+                  placeholder="Password"
+                  type="password"
+                />
+              </li>
+              <input type="submit" />
+            </ul>
+          </form>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <form autoComplete="off" onSubmit={this.loginHandler}>
+            <ul>
+              <li>
+                <input
+                  onChange={this.handleChange}
+                  name="username"
+                  placeholder="Username"
+                />
+              </li>
+              <li>
+                <input
+                  onChange={this.handleChange}
+                  name="password"
+                  placeholder="Password"
+                  type="password"
+                />
+              </li>
+              <input type="submit" />
+            </ul>
+          </form>
+        </div>
+      );
+    }
   }
 }
 // }
